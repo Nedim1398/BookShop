797260ed50e26fc3a633fa098def5e6b74702bba
Nedim1398 <35222169+Nedim1398@users.noreply.github.com>
2019-02-12 17:37:14 +0100

    Add files via upload

--- cart.php
+++ cart.php
@@ -72,11 +72,36 @@ function pre_r($array){
 		<link rel="stylesheet" href="cart.css"/>
 	</head>
 	<body>
-		<div class="container">
-		<?php
+		<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <a class="navbar-brand" href="http://localhost/shop/about.php">Bookworm Adventures</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/shop/home.php">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="http://localhost/shop/cart.php">Checkout <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0" name="form" method="post">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="search" name="search" id="search">
+	  <button class="btn btn-info" type="submit">Search</button>
+	</form>
+    
+  </div>
+</nav>
+		<div class="container" id="div">
+		<?php 
 		$connect = mysqli_connect('localhost','root','','cart');
-		$query = 'SELECT * FROM products ORDER by id ASC';
+		if(!empty($_POST['search'])){
+			$search = $_POST['search'];
+			$search = (string)$search;
+			$query = "SELECT * FROM products WHERE name LIKE '%$search%' ORDER by id ASC";
+		}else $query = 'SELECT * FROM products ORDER by id ASC';
 		$result = mysqli_query($connect,$query);
 
 		if($result):
@@ -159,5 +184,7 @@ function pre_r($array){
 		</table>
 		</div>			
 		</div>
+		<script src="cart.js"></script> 
+		<script type="text/javascript" src="jquery-3.3.1.min.js"></script>
 	</body>
 </html>
