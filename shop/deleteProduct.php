5d593d640cf1b3dc779da9c3ffea846901e3e557
Nedim1398 <35222169+Nedim1398@users.noreply.github.com>
2019-04-06 23:23:19 +0200

    Add files via upload

---
+++ shop/deleteProduct.php
@@ -0,0 +1,20 @@
+<?php
+			$name = $_POST['name'];
+			$name = (string)$name;
+			$price = $_POST['price'];
+			$price = (float)$price;
+			
+		$connect2 = mysqli_connect('localhost','root','','cart');
+		$query2 = "SELECT image FROM products WHERE name='$name'";
+		$result = mysqli_query($connect2,$query2);
+		$row = mysqli_fetch_array($result);
+		$image = $row['image'];
+		echo $image;
+		$path='C:/xampp/htdocs/shop/';
+		unlink($path.$image);
+		
+		$connect = mysqli_connect('localhost','root','','cart');
+		$query = "DELETE FROM products WHERE name='$name'";
+		mysqli_query($connect,$query);
+		header("Location: http://localhost/shop/cartLog.php");
+?>
---
+++ shop/editProduct.php
@@ -0,0 +1,38 @@
+<?php
+	//Get new values
+	$name = $_POST['name'];
+	$name = (string)$name;
+	$price = $_POST['price'];
+	$price = (float)$price;
+	
+	//Get old values
+	$nameH = $_POST['nameH'];
+	$nameH = (string)$nameH;
+	$priceH = $_POST['priceH'];
+	$priceH = (float)$priceH;
+	$imgUploaded = basename($_FILES["fileToUpload"]["name"]);
+	
+	//Check if an image was added
+  		if(!(empty($imgUploaded))){
+			$connect2 = mysqli_connect('localhost','root','','cart');
+			$query2 = "SELECT image FROM products WHERE name='$nameH'";
+			$result = mysqli_query($connect2,$query2);
+			$row = mysqli_fetch_array($result);
+			
+			//Deletes current image and reuploads new one
+			$image = $row['image'];
+			echo $image;
+			$path='C:/xampp/htdocs/shop/';
+			unlink($path.$image);
+			include('C:/xampp/htdocs/shop/reupload.php');
+			
+			$connect = mysqli_connect('localhost','root','','cart');
+			$query = "UPDATE products SET name='$name', price='$price', image='$imgUploaded' WHERE name='$nameH'";
+			mysqli_query($connect,$query);
+		}else{
+			$connect = mysqli_connect('localhost','root','','cart');
+			$query = "UPDATE products SET name='$name', price='$price' WHERE name='$nameH'";
+			mysqli_query($connect,$query);
+		}
+		header("Location: http://localhost/shop/cartLog.php");
+?>
